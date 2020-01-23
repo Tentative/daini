0d2d910a46f978a7a50d86a9eb207331a5e48e06
Tentative <micoli.giacomo@gmail.com>
2020-01-23 19:54:19 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,17 @@
-const resolve = dir => require('path').join(__dirname, dir);
+const resolve = dir => require("path").join(__dirname, dir);
 
 module.exports = {
-	transpileDependencies: [
-		'vue-echarts',
-		'resize-detector'
-	],
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/frog-admin/'
-		: '/',
-	chainWebpack: (config) => {
-		config.resolve.alias
-			.set('@', resolve('src'))
-	},
+  transpileDependencies: ["vue-echarts", "resize-detector"],
+  // publicPath: process.env.NODE_ENV === 'production'
+  // 	? '/frog-admin/'
+  // 	: '/',
+  publicPath: "/",
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src"));
+  },
 
-	devServer: {
-		port: 9999,
-
-	},
-	runtimeCompiler: true
+  devServer: {
+    port: 9999
+  },
+  runtimeCompiler: true
 };
