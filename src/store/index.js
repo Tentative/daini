ece2bbfec0b250cef58d44de4900e11c56c50b28
root <root@DESKTOP-QDCPP1L.localdomain>
2020-02-10 06:07:49 +0100

    fix try

--- src/store/index.js
+++ src/store/index.js
@@ -1,89 +1,92 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
-import getters from './getters'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import getters from "./getters";
+import axios from "axios";
+import { queueScheduler } from "rxjs";
 
 Vue.use(Vuex);
 
-axios.defaults.baseURL = 'https://data.reevo.io';
+axios.defaults.baseURL = "https://data.reevo.io";
 
-const modulesFiles = require.context('./modules', false, /\.js$/);
+const modulesFiles = require.context("./modules", false, /\.js$/);
 
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, "$1");
   const value = modulesFiles(modulePath);
   modules[moduleName] = value.default;
-  return modules
+  return modules;
 }, {});
 
 const _store = new Vuex.Store({
   state: {
-    status: '',
-    token: localStorage.getItem('token') || '',
-    user: {
-      NomeUtente: '',
-      Password: '',
-      CodiceRichiesta: '',
-      IsMemorizzaPassword: null,
-      CodiceClient: 'reevolacerba2020',
-      UserAgent: 'test',
-      IndirizzoIP: 'test',
-      VersioneClient: 'test'
-    },
+    status: "",
+    token: localStorage.getItem("token") || "",
+    CodiceRichiesta: "",
+    ipUtente: "127.0.0.0",
+    userAgentUtente: "user agent test",
+    url: "app.reevo.io/login",
+    jwtUtente: null,
+    login: {
+      NomeUtente: "",
+      Password: "",
+      IsMemorizzaPassword: null
+    }
   },
   mutations: {
-    auth_request(state) {
-      state.status = 'loading'
+    auth_request(state, login) {
+      state.status = "loading";
+      state.login = login;
+      state.CodiceRichiesta = "Login";
     },
     auth_success(state, token, user) {
-      state.status = 'success'
-      state.token = token
-      state.user = user
+      state.status = "success";
+      state.token = token;
+      state.user = user;
     },
     auth_error(state) {
-      state.status = 'error'
+      state.status = "error";
     },
     logout(state) {
-      state.status = ''
-      state.token = ''
+      state.status = "";
+      state.token = "";
     },
     request(state) {
-      state.CodiceRichiesta = ''
+      state.CodiceRichiesta = "";
     },
     keepLogged(state) {
-      state.IsMemorizzaPassword = ''
+      state.IsMemorizzaPassword = "";
     }
   },
   actions: {
-    login({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit('auth_request')
-        axios({ url: "/", method: 'GET', headers: {
-          "Content-Type" : "application/x-www-form-urlencoded"
-        }, data: {
-          NomeUtente: this.state.NomeUtente,
-          Password: this.state.Password,
-          CodiceRichiesta: this.state.CodiceRichiesta
-        }} )
-          .then(resp => {
-            const token = resp.data.access_token
-            const user = resp.data.user
-            console.log("Codice risposta: " + resp.data.CodiceRisposta)
-            localStorage.setItem('token', token)
-            axios.defaults.headers.common['Authorization'] = token
-            if (resp.data.CodiceRIsposta == 'Ok') {
-            commit('auth_success', token, user) }
-            resolve(resp)
-            console.log(resp)
-          })
-          .catch(err => {
-            commit('auth_error')
-            localStorage.removeItem('token')
-            reject(err)
-          })
-      })
-    },
+    // login({ commit }, user) {
+    //   return new Promise((resolve, reject) => {
+    //     commit('auth_request')
+    //     axios({ url: "/", method: 'GET', headers: {
+    //       "Content-Type" : "application/x-www-form-urlencoded"
+    //     }, data: {
+    //       NomeUtente: this.state.NomeUtente,
+    //       Password: this.state.Password,
+    //       CodiceRichiesta: this.state.CodiceRichiesta
+    //     }} )
+    //       .then(resp => {
+    //         const token = resp.data.access_token
+    //         const user = resp.data.user
+    //         console.log("Codice risposta: " + resp.data.CodiceRisposta)
+    //         localStorage.setItem('token', token)
+    //         axios.defaults.headers.common['Authorization'] = token
+    //         if (resp.data.CodiceRIsposta == 'Ok') {
+    //         commit('auth_success', token, user) }
+    //         resolve(resp)
+    //         console.log(resp)
+    //       })
+    //       .catch(err => {
+    //         commit('auth_error')
+    //         localStorage.removeItem('token')
+    //         reject(err)
+    //       })
+    //   })
+    // },
     requestALL() {
       this.$axios.get("/", {
         CodiceClient: "reevolacerba2020",
@@ -94,24 +97,48 @@ const _store = new Vuex.Store({
         JsonWebToken: jwtUtente,
         CodiceRichiesta: state.CodiceRichiesta,
         JsonRichiesta: JSON.stringify(Richiesta)
-
-      })
+      });
+    },
+    login({ commit, state }, login) {
+      commit("auth_request", login);
+      let Richiesta = {
+        // CodiceClient: "reevolacerba2020",
+        // VersioneClient: "0.4.5",
+        // IndirizzoIP: state.ipUtente,
+        // UserAgent: state.userAgentUtente,
+        // Url: state.url,
+        // JsonWebToken: state.jwtUtente,
+        CodiceRichiesta: state.CodiceRichiesta,
+        JsonRichiesta: JSON.stringify(login)
+      };
+      axios({
+        url: "/",
+        method: "GET",
+        // headers: {
+        //   "Content-Type": "application/x-www-form-urlencoded"
+        // },
+        params: JSON.stringify(Richiesta)
+      }).then(res => {
+        console.log(res.config.data);
+        console.log(res);
+      });
     },
     logout({ commit }) {
       return new Promise((resolve, reject) => {
-        commit('logout')
-        localStorage.removeItem('token')
-        delete axios.defaults.headers.common['Authorization']
-        resolve()
-      })
+        commit("logout");
+        localStorage.removeItem("token");
+        delete axios.defaults.headers.common["Authorization"];
+        resolve();
+      });
     }
   },
   modules,
   getters,
-  plugins: [createPersistedState({
-    key: 'frog-admin'
-  })]
+  plugins: [
+    createPersistedState({
+      key: "frog-admin"
+    })
+  ]
 });
-
 
 export default _store;
