43b0b071169ba3238db722fb431ff1fb2b204170
Tentative <micoli.giacomo@gmail.com>
2020-02-14 02:56:30 +0100

    working keep logged option

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,14 +1,15 @@
 export default {
-	appTitle: state => state.apps.title,
+  appTitle: state => state.apps.title,
 
-	sidebarOpen: state => state.apps.sidebar.open,
-	tipbarOpen: state => state.apps.tipbar.open,
+  sidebarOpen: state => state.apps.sidebar.open,
+  tipbarOpen: state => state.apps.tipbar.open,
 
-	role: state => state.permission.role,
+  role: state => state.permission.role,
 
-	routes: state => state.permission.permittedRoutes,
-	showRole: state => state.authenticated,
-	isLoggedIn: state => !!sessionStorage.getItem('jwtUtente'),
-	isSession: state => state.login.IsMemorizzaPassword,
-	authStatus: state => state.status
-}
+  routes: state => state.permission.permittedRoutes,
+  showRole: state => state.authenticated,
+  isLoggedIn: state =>
+    state.jwtUtente || sessionStorage.getItem("jwtUtente") != null,
+  isSession: state => state.login.IsMemorizzaPassword,
+  authStatus: state => state.status
+};
