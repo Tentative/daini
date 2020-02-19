77863b855d7bb324b68494e364378d630db3848f
Tentative <micoli.giacomo@gmail.com>
2020-02-19 18:44:37 +0100

    table layout

--- src/store/index.js
+++ src/store/index.js
@@ -148,7 +148,7 @@ const _store = new Vuex.Store({
       return new Promise((resolve, reject) => {
         commit("auth_request", login);
         var Richiesta = {
-          VersioneClient: "0.7.9",
+          VersioneClient: "0.8.0",
           IndirizzoIP: state.ipUtente,
           UserAgent: state.userAgentUtente,
           Url: state.url,
--- src/views/common/Amazon/index.vue
+++ src/views/common/Amazon/index.vue
@@ -1,11 +1,51 @@
 <template>
   <table class="amz">
     <tr>
-      <th>#</th>
+      <th></th>
+      <th>Name</th>
+      <th>AMZ Price</th>
+      <th>Discount</th>
+      <th>In stock</th>
+      <th>Rank</th>
+      <th>FT</th>
+      <th>BB</th>
+      <th>Neg. reviews</th>
     </tr>
     <tr v-for="item in items">
       <td class="thumb"><img :src="item.UrlImmagine" /></td>
-      <td class="item">{{ item.NomeItem }}</td>
+      <td class="item">
+        <span>{{ item.NomeItem }} </span>
+      </td>
+      <td class="price">
+        <span>{{ item.PrezzoAMZ }} €</span>
+      </td>
+      <td class="discount">
+        <span v-if="item.Sconto != '0'">{{ item.Sconto }} %</span>
+        <span v-else>/</span>
+      </td>
+      <td class="stock">
+        <span v-if="item.InStockAMZ == 'No'"
+          ><i class="fas fa-times-circle"></i
+        ></span>
+      </td>
+      <td class="rank">
+        <span>{{ item.SalesRank }}</span>
+      </td>
+      <td class="track">
+        <span v-if="item.FastTrack == 'Oggi'">Oggi</span>
+        <span v-if="item.FastTrack == 'Giorno_1'">1 Giorno</span>
+        <span v-if="item.FastTrack == 'Si'"><i class="fas fa-check"></i></span>
+        <span v-if="item.FastTrack == 'No'"
+          ><i class="fas fa-times-circle"></i
+        ></span>
+      </td>
+      <td class="buybox">
+        <span v-if="item.IsBuyBox"><i class="fas fa-check"></i></span>
+        <span v-else><i class="fas fa-times-circle"></i></span>
+      </td>
+      <td class="reviews">
+        <span>{{ item.NegativeReviewsPercentuale }} %</span>
+      </td>
     </tr>
   </table>
 </template>
@@ -59,6 +99,7 @@ export default {
 
           this.items.push(image[ListaItem]);
           this.urls.push(image[ListaItem].UrlImmagine);
+          console.log(image[ListaItem].InStock);
         }
         this.items.splice(-2);
         console.log(image);
@@ -68,6 +109,16 @@ export default {
 };
 </script>
 <style lang="scss">
+th,
+td {
+  text-align: center;
+  padding: 0 20px 0 0;
+}
+
+td.item {
+  text-align: left;
+}
+
 .amz {
   border: 1px solid red;
 }
@@ -75,5 +126,21 @@ export default {
 .thumb img {
   width: 60px;
   height: 60px;
+}
+.item {
+  max-width: 200px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.fas,
+.fa-times-circle {
+  color: red;
+}
+
+.fas,
+.fa-check {
+  color: green;
 }
 </style>
